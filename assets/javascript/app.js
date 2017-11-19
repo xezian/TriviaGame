ff761b11cc923ca5fcacbcb7d85d64e60b6443af
xezian <xezian@icloud.com>
2017-11-19 15:05:07 -0700

    more pseudocode and general rambling

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,8 +12,7 @@
 
 // var score (as a percentage?)
 
-
-// create functions that can be called to run the things the game does
+// create functions that can be called to run the things the game does like especially the jQuery stuff that interacts with the DOM
 
 // put the game itself into an object? Is this a good idea? look at some examples first
 
@@ -22,3 +21,13 @@
 // Lets go over the game structure itself. I imagine user will want to click a start game option first
 
 // when start game is clicked let's have that run a function that is the game itself, perhaps inside of an object so it can refer to it's own stuff easier? or maybe as a function that calls the other functions of the game and draws information from the objects representing the game's content.
+
+// there will now be a setTimeout called inside which we run the functions do the game stuff
+
+// the setTimeout will be stored in a variable so it can have clearTimeout called on it by selecting an answer
+
+// once past the timeout one way or another, we need to show the answer, and call a setTimeout on that, and we need to show if the answer was correct, thereby adding to the correct, incorrect or unanswered counters
+
+// I'd like to pull these questions at random out of an array, with a reset function that resets the array. maybe if it pulls them out and adds them to a new array, of askedQuestions or someShit, then they can just get returned to the original array after the game is over instead of redefining the whole thing again which would also be easy though I think. function populateArray? (clears the array and fills it up with the original questions set) I do want practice though playing with arrays, so maybe I don't know whatever I'll try someShit.
+
+// I will also say I believe it will be a nicer app, and more re-usable, the more easily I can switch out the content by which I mean the actual questions and answers.
