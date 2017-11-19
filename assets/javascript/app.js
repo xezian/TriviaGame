879c4ecb032f39527610b11241a2471a22d3cff3
xezian <xezian@icloud.com>
2017-11-19 14:49:47 -0700

    pseudocode

---
+++ assets/javascript/app.js
@@ -0,0 +1,24 @@
+// Timed Trivia Questions (HW Week 5 UA Coding Bootcamp)
+
+// create an array of questions that are objects with false and correct answers as properties of the object
+
+// declare variables as needed to make this thing tick
+
+// var correct answer counter
+
+// var incorrect answer counter
+
+// var unanswered questions
+
+// var score (as a percentage?)
+
+
+// create functions that can be called to run the things the game does
+
+// put the game itself into an object? Is this a good idea? look at some examples first
+
+// I want to set up the game itelf as simplified as possible this time so that it can by run by calling functions instead of a giant mess of code like the last game. I will need to use setTimeout and setInterval functions which are new to me so I need to keep everything as tidy as possible around that
+
+// Lets go over the game structure itself. I imagine user will want to click a start game option first
+
+// when start game is clicked let's have that run a function that is the game itself, perhaps inside of an object so it can refer to it's own stuff easier? or maybe as a function that calls the other functions of the game and draws information from the objects representing the game's content.
