b84bf5cca58c17e5a74d6552f0de3853a240288f
xezian <xezian@icloud.com>
2017-11-22 12:10:11 -0700

    added code to app.js, added question-vault.js as a seperate file to store the questions, put the whole game into an object, began declaring all the functions needed to make it run

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,21 +1,102 @@
 // Timed Trivia Questions (HW Week 5 UA Coding Bootcamp)
 
-// create an array of questions that are objects with false and correct answers as properties of the object
+// create an array of questions that are objects with false and correct answers as properties of the object. How do I put this in a seperate file? note: I have attempted this.
 
+// put the entire game into an object. Is this a good idea? Let's find out!
+var theGameItself = {
 // declare variables as needed to make this thing tick
-
+        todaysQuestions: [],
 // var correct answer counter
-
+        correctAnswers: 0,
 // var incorrect answer counter
-
+        incorrectAnswers: 0,
 // var unanswered questions
-
+        unansweredQuestions: 0,
 // var score (as a percentage?)
-
+        score: 0,
 // create functions that can be called to run the things the game does like especially the jQuery stuff that interacts with the DOM
-
-// put the game itself into an object? Is this a good idea? look at some examples first
-
+        resetScore: function() {
+        // reset all the game values
+                this.correctAnswers = 0;
+                this.incorrectAnswers = 0;
+                this.unansweredQuestions = 0;
+                this.score = 0;
+        },
+        createDivs: function() {
+// This is where I create all of the html divs that will be used to display the game content. I think it's probably harder to do it this way and with no obvious advantages, but I wanted to see if it would work. See how little html I have in my index.html file! 
+// every div declared
+                var header = $("<div/>");
+                var footer = $("<footer/>");
+                var outer = $("<div/>");
+                var inner = $("<div/>");
+                var startGameButton = $("<button>");
+// header div id and class          
+                header.attr("id", "header");
+                header.addClass("row justify-content-center");
+                header.html("<h1>Welcome to JavaScript Trivia!</h4>");
+// footer div id and class
+                footer.attr("id", "footer");
+                footer.addClass("row justify-content-center");
+                footer.css({"position": "absolute", "bottom": "0", "width": "100%", "height": "5vh", "text-align": "center"});
+                footer.html("© Copyright 2017 Jason A. Leo");
+// outer div id and class
+                outer.attr("id", "outer-div");
+                outer.addClass("container-fluid");
+// inner div id and class            
+                inner.attr("id", "inner-div");
+                inner.addClass("row container-fluid justify-content-center");
+// start game button id and class
+                startGameButton.attr("id", "start");
+                startGameButton.addClass("btn btn-primary");
+                startGameButton.html("START!");
+// append all divs
+                header.appendTo("body");
+                outer.appendTo($("body"));
+                footer.appendTo("body");
+                inner.appendTo($(outer));
+                startGameButton.appendTo(inner);
+        },
+// function whose job it is to initialize the game!
+        gameInit: function() {
+// here I copy the questions from my questionVault array (in question-vault.js) into the game file. I want to do this so I can more easily switch out the questions, even store new content in another file and link to that instead
+                theGameItself.todaysQuestions = questionVault.slice();
+// now it's time to set up the Document Object Model again and reset the score
+                $("body").empty();
+                theGameItself.createDivs();
+                theGameItself.resetScore();
+        },
+        newQuestion: function() {
+                $("#inner-div").empty();
+                var index = theGameItself.todaysQuestions.indexOf(theGameItself.todaysQuestions[Math.floor(Math.random() * theGameItself.todaysQuestions.length)]);
+                var questionPkg = theGameItself.todaysQuestions.splice(index, 1);
+                var question = $("<div/>");
+                question.attr("id", "question")
+                question.attr("correct-answer", questionPkg[0].correctAnswer());
+                question.attr("class", "row container-fluid justify-content-center");
+                question.html(questionPkg[0].question);
+                var answers = $("<div/>");
+                answers.attr("class", "row container-fluid justify-content-center");
+                for (var i = 0; i < questionPkg[0].answers.length; i++) {
+                        var answer = $("<button>");
+                        answer.attr("class", "btn btn-info")
+                        answer.attr("index", questionPkg[0].answers.indexOf(questionPkg[0].answers[i]));
+                        answer.html(questionPkg[0].answers[i]);
+                        answers.append(answer);
+                }
+                $("#inner-div").append(question);
+                $("#inner-div").append(answers);
+                console.log(theGameItself.todaysQuestions);
+        },
+        showCorrectAnswer: function() {
+                var correctAnswerFinder = $("#question").attr("correct-answer");
+                
+        }
+};
+// on click event listener to start the game
+$(document).on("click", "#start", function(){
+        theGameItself.newQuestion();
+        theGameItself.showCorrectAnswer();
+})
 // I want to set up the game itelf as simplified as possible this time so that it can by run by calling functions instead of a giant mess of code like the last game. I will need to use setTimeout and setInterval functions which are new to me so I need to keep everything as tidy as possible around that
 
 // Lets go over the game structure itself. I imagine user will want to click a start game option first
@@ -31,3 +112,6 @@
 // I'd like to pull these questions at random out of an array, with a reset function that resets the array. maybe if it pulls them out and adds them to a new array, of askedQuestions or someShit, then they can just get returned to the original array after the game is over instead of redefining the whole thing again which would also be easy though I think. function populateArray? (clears the array and fills it up with the original questions set) I do want practice though playing with arrays, so maybe I don't know whatever I'll try someShit.
 
 // I will also say I believe it will be a nicer app, and more re-usable, the more easily I can switch out the content by which I mean the actual questions and answers.
+$(document).ready(function() {
+        theGameItself.gameInit();
+});
---
+++ assets/javascript/question-vault.js
@@ -0,0 +1,155 @@
+var questionVault = [
+        {
+            question: "Who wrote the first version of JavaScript?",
+            answers: ["Peter Dinklage", "Mary Poppins", "Brendan Eich", "Benjamin Franklin"],
+            correctAnswer: function() {
+                return this.answers[2];
+            },
+            image: "http://placehold.it/200",
+            alt: "picture of Brendan Eich"
+        },
+        {
+            question: "Which one will return a random number between greater than or equal to zero and less than 10?",
+            answers: ["Math.floor(Math.random() * 10)", "Math.ceil(Math.random() * 10)", "Math.floor(Math.random(0,10))", "Math.random(10)"],
+            correctAnswer: function() {
+                return this.answers[0];
+            },
+            image: "http://placehold.it/200",
+            alt: "Very random picture"
+        },
+        {
+            question: "Which one is 'A'",
+            answers: ["E", "W", "A", "F"],
+            correctAnswer: function() {
+                return this.answers[2];
+            },
+            image: "http://placehold.it/200",
+            alt: "A"
+        },
+        {
+            question: "Which one is 'F'",
+            answers: ["E", "W", "A", "F"],
+            correctAnswer: function() {
+                return this.answers[3];
+            },
+            image: "http://placehold.it/200",
+            alt: "A"
+        },
+        {
+            question: "Which one is 'E'",
+            answers: ["E", "W", "A", "F"],
+            correctAnswer: function() {
+                return this.answers[0];
+            },
+            image: "http://placehold.it/200",
+            alt: "A"
+        },
+        {
+            question: "Which one is 'W'",
+            answers: ["E", "W", "A", "F"],
+            correctAnswer: function() {
+                return this.answers[1];
+            },
+            image: "http://placehold.it/200",
+            alt: "A"
+        },
+        {
+            question: "Which one is 'R'",
+            answers: ["E", "R", "A", "F"],
+            correctAnswer: function() {
+                return this.answers[1];
+            },
+            image: "http://placehold.it/200",
+            alt: "A"
+        },
+        {
+            question: "Which one is 'L'",
+            answers: ["P", "W", "A", "L"],
+            correctAnswer: function() {
+                return this.answers[3];
+            },
+            image: "http://placehold.it/200",
+            alt: "A"
+        },
+        {
+            question: "Which one is 'Q'",
+            answers: ["E", "W", "Q", "F"],
+            correctAnswer: function() {
+                return this.answers[2];
+            },
+            image: "http://placehold.it/200",
+            alt: "A"
+        },
+        {
+            question: "Which one is 'S'",
+            answers: ["S", "W", "A", "F"],
+            correctAnswer: function() {
+                return this.answers[0];
+            },
+            image: "http://placehold.it/200",
+            alt: "A"
+        },
+        {
+            question: "Which one is 'J'",
+            answers: ["E", "W", "A", "J"],
+            correctAnswer: function() {
+                return this.answers[3];
+            },
+            image: "http://placehold.it/200",
+            alt: "A"
+        },
+        {
+            question: "Which one is 'K'",
+            answers: ["E", "W", "K", "F"],
+            correctAnswer: function() {
+                return this.answers[2];
+            },
+            image: "http://placehold.it/200",
+            alt: "A"
+        },
+        {
+            question: "Which one is 'N'",
+            answers: ["N", "W", "A", "F"],
+            correctAnswer: function() {
+                return this.answers[0];
+            },
+            image: "http://placehold.it/200",
+            alt: "A"
+        },
+        {
+            question: "Which one is 'X'",
+            answers: ["E", "X", "A", "F"],
+            correctAnswer: function() {
+                return this.answers[1];
+            },
+            image: "http://placehold.it/200",
+            alt: "A"
+        },
+        {
+            question: "Which one is 'M'",
+            answers: ["E", "W", "A", "M"],
+            correctAnswer: function() {
+                return this.answers[3];
+            },
+            image: "http://placehold.it/200",
+            alt: "A"
+        },
+        {
+            question: "Which one is 'B'",
+            answers: ["E", "B", "R", "K"],
+            correctAnswer: function() {
+                return this.answers[1];
+            },
+            image: "http://placehold.it/200",
+            alt: "A"
+        },
+        {
+            question: "Which one is 'P'",
+            answers: ["P", "W", "A", "F"],
+            correctAnswer: function() {
+                return this.answers[0];
+            },
+            image: "http://placehold.it/200",
+            alt: "A"
+        },
+    ]
